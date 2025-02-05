686f9aefdce2b76314350237cdeff6a18d8ebdc3
Herbert Tamayo <hftamayo@gmail.com>
2025-02-05 12:04:25 -0600

    relocating role seeder command

--- src/services/commands/dataseeding/seedRolesCommand.ts
+++ src/services/commands/dataseeding/seedRolesCommand.ts
@@ -1,4 +1,4 @@
-import seedRoles from '../../shared/utils/dataseed/seedRoles';
+import seedRoles from '../../../shared/utils/dataseed/seedRoles';
 
 export class SeedRolesCommand {
   async execute(): Promise<void> {
