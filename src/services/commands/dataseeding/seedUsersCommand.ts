8d18bdc8975f341e74b8049d8549ec923e2aad22
Herbert Tamayo <hftamayo@gmail.com>
2025-03-11 14:37:22 -0600

    updating to seedUser function name

--- src/services/commands/dataseeding/seedUsersCommand.ts
+++ src/services/commands/dataseeding/seedUsersCommand.ts
@@ -2,7 +2,7 @@ import SeedUsers from '../../../shared/utils/dataseed/SeedUsers';
 
 export class SeedUsersCommand {
   async execute(): Promise<void> {
-    const seedRolesInstance = new SeedUsers();
-    await seedRolesInstance.seedRoles();
+    const seedUsersInstance = new SeedUsers();
+    await seedUsersInstance.seedUsers();
   }
 }
