b29ad5c5b7d64a32fd2efff0b7cbf9d801105de1
Herbert Tamayo <hftamayo@gmail.com>
2025-05-13 15:13:01 -0600

    applying hcMiddleware on hcRoutes

--- src/api/v1/routes/hcRoutes.ts
+++ src/api/v1/routes/hcRoutes.ts
@@ -1,13 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { HealthCheckController } from '../controllers/HealthCheckController';
+import { healthCheckValidation } from '../middlewares/validation/healthCheck.validation';
+import { ValidationMiddleware } from '../middlewares/errorHandler.middleware';
 
 const hcRouter = express.Router();
 const healthCheckController = new HealthCheckController();
 
-hcRouter.get('/app', (req, res) =>
-  healthCheckController.appHealthCheck(req, res),
+hcRouter.get(
+  '/app',
+  healthCheckValidation,
+  ValidationMiddleware.validate,
+  async (req: Request, res: Response): Promise<void> => {
+    await healthCheckController.appHealthCheck(req, res);
+  },
 );
-hcRouter.get('/db', (req, res) =>
-  healthCheckController.dbHealthCheck(req, res),
+hcRouter.get(
+  '/db',
+  healthCheckValidation,
+  ValidationMiddleware.validate,
+  async (req: Request, res: Response): Promise<void> => {
+    await healthCheckController.dbHealthCheck(req, res);
+  },
 );
 export default hcRouter;
